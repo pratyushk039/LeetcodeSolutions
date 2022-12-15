299554f7883e5b8231815b339fa6746c6fcf5e63
Pratyush <54001208+pratyushk039@users.noreply.github.com>
2022-12-15 23:07:11 +0530

    Time: 786 ms (70.70%), Space: 125.3 MB (75.16%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> rank,parent;
+    int find(int x){
+        if(parent[x]==x){return x;}
+        return parent[x] = find(parent[x]);
+    }
+    void uni(int x,int y){
+        int parX=find(x),parY=find(y);
+        if(rank[parX]>rank[parY]){parent[parY]=parX;}
+        if(rank[parX]<rank[parY]){parent[parX]=parY;}
+        if(rank[parX]==rank[parY]){parent[parY]=parX;rank[parX]++;}
+        return;
+    }
+    int minScore(int n, vector<vector<int>>& roads) {
+        rank.resize(n+1,1);
+        parent.resize(n+1);
+        int ans = INT_MAX;
+        for(int i=0;i<=n;i++){parent[i]=i;}
+        for(auto v:roads){
+            uni(v[0],v[1]);
+        }
+        int par = find(1);
+        for(auto vec:roads){
+            if(find(vec[0])==par || find(vec[1])==par){ans=min(ans,vec[2]);}
+        }
+        return ans;
+    }
+};
