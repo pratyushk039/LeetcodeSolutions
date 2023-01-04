c4b1a3f9cc6c4432758e2e71eca78dad91461f64
Pratyush <54001208+pratyushk039@users.noreply.github.com>
2023-01-04 21:26:43 +0530

    Time: 364 ms (53.51%), Space: 104.1 MB (38.12%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,27 @@
+bool k =true;
+vector<int> dp(1e5+1,0);
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        if(k){
+            dp[1]=INT_MAX;
+            dp[2]=1;dp[3]=1;
+            for(int i=4;i<=1e5;i++){
+                if(dp[i-3]==INT_MAX && dp[i-2]==INT_MAX){dp[i]=INT_MAX;}
+                else{
+                    dp[i] = 1+min(dp[i-3],dp[i-2]);
+                }
+            }
+        }
+        unordered_map<int,int>um;
+        for(int i:tasks){
+            um[i]+=1;
+        }
+        int ans=0;
+        for(auto itr:um){
+            if(dp[itr.second]==INT_MAX){return -1;}
+            ans+=dp[itr.second];
+        }
+        return ans;
+    }
+};
