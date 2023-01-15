6fbec86a9cc2796715aca234bc80bda43936168d
Pratyush <54001208+pratyushk039@users.noreply.github.com>
2023-01-15 10:03:52 +0530

    Time: 385 ms (80.00%), Space: 87 MB (20.00%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -5,10 +5,7 @@ public:
         int m=queries.size(),r1,r2,c1,c2;
         for(auto vec:queries){
             r1=vec[0],c1=vec[1],r2=vec[2],c2=vec[3];
-            for(int i=c1;i<=c2;i++){ans[r1][i]+=1;}
-            if(r2<n-1){
-                for(int i=c1;i<=c2;i++){ans[r2+1][i]-=1;}
-            }
+            for(int i=c1;i<=c2;i++){ans[r1][i]+=1;if(r2<n-1){ans[r2+1][i]-=1;}}
         }
         for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
