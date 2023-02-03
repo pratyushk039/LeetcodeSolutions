ea15d2a35c5250511852fd733ea99f4be0dc64e3
Pratyush <54001208+pratyushk039@users.noreply.github.com>
2023-02-04 02:42:47 +0530

    Time: 11 ms (74.80%), Space: 8.1 MB (90.36%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int start=0,n=s.size();
+        if(numRows>n){numRows=n;}
+        if(numRows==1){return s;}
+        string ans="";
+        int j;
+        for(int i=1;i<=numRows;i++){
+            if(i==1){
+                j=start;
+                while(j<n){ans.push_back(s[j]);j+=2*(numRows-1);}
+                start=1;
+                continue;
+            }
+            if(i==numRows){
+                j=numRows-1;
+                while(j<n){ans.push_back(s[j]);j+=2*(numRows-1);}
+                break;
+            }
+            int row=i;
+            j=row-1;
+            bool a=true,b=false;
+            while(j<n){
+            if(a){
+                if(j<n)ans.push_back(s[j]);
+                j+=2*(numRows-row);
+                a=false;b=true;
+            }
+            if(b){
+                if(j<n)ans.push_back(s[j]);
+                j+=2*(row-1);
+                a=true;b=false;
+            }}
+        }
+        return ans;
+    }
+};
