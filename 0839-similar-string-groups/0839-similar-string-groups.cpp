2bbe8162e4382cb9722704be07b1991d7234380c
Pratyush <54001208+pratyushk039@users.noreply.github.com>
2023-04-28 22:25:07 +0530

    Time: 1290 ms (5.07%), Space: 479.2 MB (5.40%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> parent,rank;
+    int n;
+    int find(int x){
+        if(parent[x]==x){return x;}
+        return parent[x] = find(parent[x]);
+    }
+    void Union(int x,int y){
+        int parx=find(x),pary=find(y);
+        if(rank[parx]>rank[pary]){
+            parent[pary] = parx;
+        }
+        else if(rank[parx]<rank[pary]){
+            parent[parx] = pary;
+        }
+        else{
+            parent[parx]=pary;
+            rank[pary]+=1;
+        }
+    }
+    bool check(const string &s3, const string &s4){
+        string s1=s3,s2=s4;
+        if(s1==s2){return true;}
+        int m =s1.size();
+        int idx=0;
+        vector<int> idc;
+        for(int i=0;i<m;i++){
+            if(s1[i]!=s2[i]){idx++;idc.push_back(i);}
+        }
+        if(idx!=2){return false;}
+        swap(s1[idc[0]],s1[idc[1]]);
+        return s1==s2;
+    }
+    int numSimilarGroups(vector<string>& strs) {
+        this->n = strs.size();
+        parent.resize(n);rank.resize(n,0);
+        for(int i=0;i<n;i++){parent[i]=i;}
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(check(strs[i],strs[j])){
+                    Union(i,j);
+                }
+            }
+        }
+        unordered_set<int> set;
+        for(int i=0;i<n;i++){
+            set.insert(find(i));
+        }
+        return set.size();
+    }
+};
