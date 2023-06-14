4abe022851860bad5be5aa33a2f0e10bedb2fa1d
Pratyush <54001208+pratyushk039@users.noreply.github.com>
2023-06-14 21:52:34 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (28.42%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,3 +1,4 @@
+unordered_map<long long int,int> umap;
 class Solution {
 public:
     int util(long long n){
@@ -6,16 +7,28 @@ public:
         }
         int a=INT_MAX,b=INT_MAX,c=INT_MAX;
         if(n%2==0){
-            a = 1+util(n/2);
+            if(umap.find(n/2)==umap.end()){
+                umap[n/2]=util(n/2);
+            }
+            a=1+umap[n/2];
         }
         else{
-            b = 1+util(n-1);
-            c = 1+util(n+1);
+            if(umap.find(n+1)==umap.end()){
+                umap[n+1] = util(n+1);
+            }
+            if(umap.find(n-1)==umap.end()){
+                umap[n-1] = util(n-1);
+            }
+            b = 1+umap[n+1];
+            c = 1+umap[n-1];
             b=min(b,c);
         }
         return min(a,b);
     }
     int integerReplacement(int n) {
-        return util(1ll*n);
+        if(umap.find(n)==umap.end()){
+        return umap[n] = util(1ll*n);
+        }
+        else return umap[n];
     }
 };
