81e637d509a635f11fad037ecc275084da0ec4e4
Pratyush <54001208+pratyushk039@users.noreply.github.com>
2023-09-17 19:00:52 +0530

    Time: 6 ms (100.00%), Space: 21.9 MB (33.33%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.cpp
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    int setk(int i, int k){
-        int ans=0;
-        for(int j=0;j<31;j++){
-            if((1<<j)&i){ans+=1;}
-        }
-        return ans==k;
-    }
     int sumIndicesWithKSetBits(vector<int>& nums, int k) {
         int ans=0;
-        for(int i=0;i<nums.size();i++){
-            if(setk(i,k)){ans+=nums[i];}
+        for(int j=0;j<nums.size();j++){
+            int cnt=0,i=j;
+            while(i!=0){
+                cnt+=1;
+                i=i&(i-1);
+            }
+            if(cnt==k){ans+=nums[j];}
         }
         return ans;
     }
